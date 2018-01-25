2e26e1edf193e21a8486f5d7edeab36f8f787a2d
baudekin <michael.bodkin@hitachivantara.com>
2018-01-25 13:02:15 -0500

    Added bean breaking exmaple.

---
+++ spark-etl/src/main/java/com/github/spark/etl/SalesDataExtendedBean.java
@@ -0,0 +1,28 @@
+package com.github.spark.etl.engine;
+
+public class SalesDataExtendedBean {
+  private Integer ORDERNUMBER;
+  private Integer QUANTITYORDERED;
+  private Double PRICEEACH;
+  private Integer ORDERLINENUMBER;
+  private Integer SALES;
+  private String ORDERDATE;
+  private String STATUS;
+  private Integer QTR_ID;
+  private Integer MONTH_ID;
+  private Integer YEAR_ID;
+  private String PRODUCTLINE;
+  private Integer MSRP;
+  private String PRODUCTCODE;
+  private String CUSTOMERNAME;
+  private String PHONE;
+  private String ADDRESSLINE1;
+  private String ADDRESSLINE2;
+  private String CITY;
+  private String STATE;
+  private String POSTALCODE;
+  private String COUNTRY;
+  private String TERRITORY;
+  private String CONTACTLASTNAME;
+  private String CONTACTFIRSTNAME;
+}
